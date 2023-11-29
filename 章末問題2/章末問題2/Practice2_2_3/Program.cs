4b91b233b230526d3e940b2e87bec19ebc001cfd
BEING\hatta <hatta@beingcorp.co.jp>
2023-11-29 13:19:12 +0900

    #457409　[C#テキスト]1章演習問題

---
+++ 章末問題2/章末問題2/Practice2_2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Practice2_2_3 {
+    class Program {
+        //「2.2:売り上げ集計プログラム」で作成したプログラムを変更し、商品カテゴリ別の売上高を求めるプログラムを作成してください。
+        static void Main(string[] args) {
+            var wSales = new SalesCounter("sales.csv");
+            var wCalculateSalesPerCategorye = wSales.GetPerCategorySales();
+
+            foreach (var wObj in wCalculateSalesPerCategorye) {
+                Console.WriteLine("{0} {1}", wObj.Key, wObj.Value);
+            }
+        }
+    }
+}
---
+++ 章末問題2/章末問題2/Practice2_2_3/SalesCounter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Practice2_2_3 {
+    //売上集計クラス
+    class SalesCounter {
+        private IEnumerable<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(string vFilePath) {
+            _sales = ReadSales(vFilePath);
+        }
+
+        //売り上げのデータを読み込み、Saleオブジェクトのリストを返す
+        private static IEnumerable<Sale> ReadSales(string vFilePath) {
+            var wSales = new List<Sale>();
+            var wLines = File.ReadAllLines(vFilePath);
+            foreach (var wLine in wLines) {
+                string[] wItems = wLine.Split(',');
+                Sale wSale = new Sale {
+                    ShopName = wItems[0],
+                    ProductCategory = wItems[1],
+                    Amount = int.Parse(wItems[2])
+                };
+                wSales.Add(wSale);
+            }
+            return wSales;
+        }
+
+        //商品別売り上げを求める
+        public IDictionary<string, int> GetPerCategorySales() {
+            var wDict = new Dictionary<string, int>();
+            foreach (var wSale in _sales) {
+                if (wDict.ContainsKey(wSale.ProductCategory))
+                    wDict[wSale.ProductCategory] += wSale.Amount;
+                else
+                    wDict[wSale.ProductCategory] = wSale.Amount;
+            }
+            return wDict;
+        }
+    }
+}
