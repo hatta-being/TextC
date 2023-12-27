cfff4ff2961230aa444c79f03251b2f5ca81eb89
BEING\hatta <hatta@beingcorp.co.jp>
2023-12-27 11:18:43 +0900

    #464428 [C#テキスト]4章演習問題

---
+++ 章末問題4/章末問題4/章末問題4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+namespace 章末問題4 {
+    class Program {
+        static void Main(string[] args) {
+            /*問題4.2
+            1.YearMonthを要素に持つ配列を定義し、初期値として5つのYearMonthオブジェクトをセットしてください。*/
+            YearMonth[] w21stCenturyData = {
+        new YearMonth(2021, 1),
+        new YearMonth(2021, 5),
+        new YearMonth(2022, 8),
+        new YearMonth(2023, 12),
+        new YearMonth(2100, 2)
+        };
+
+            //2.この配列の要素(YearMonth)オブジェクトをすべて列挙し、その値をコンソールに出力してください。
+            foreach (YearMonth wYearMonth in w21stCenturyData) {
+                Console.WriteLine($"{wYearMonth.Year.ToString()}年{wYearMonth.Month}月");
+            }
+
+            /*4.3で作成したメソッドを呼び出し、最初に見つかった21世紀のデータの年を表示してください。
+            見つからなければ、"21世紀のデータはありません"を表示してください。*/
+            YearMonth wFound21 = GetFirst21(w21stCenturyData);
+            Console.WriteLine(wFound21 != null ? $"{wFound21.Year}年{wFound21.Month}月" : "21世紀のデータはありません");
+
+
+            /*5.配列に格納されているすべてのYearMonthの1ヵ月後を求め、その結果を新たな配列に入れてください。
+            その後、その配列の要素の内容(年月)を順に表示してください。LINQを使えるところはLINQを使って実装してみてください。*/
+            // LINQを使用して1ヶ月後のYearMonthを計算し、新しい配列に格納
+            var wNextMonthArray = w21stCenturyData.Select(x => x.AddOneMonth(1)).ToArray();
+
+            // LINQを使用して年月を順に並び変え
+            var wSortedArray = wNextMonthArray.OrderBy(x => x.Year).ThenBy(x => x.Month).ToArray();
+
+            // 結果を順に表示
+            Console.WriteLine();
+            Console.WriteLine("1ヶ月後かつ年月順の結果:");
+            foreach (var x in wSortedArray) {
+                Console.WriteLine(x);
+            }
+        }
+
+        /*3.配列の中の最初に見つけた21世紀のYearMonthオブジェクトを返すメソッドを書いてください。
+        見つからなかった場合は、nullを返してください。foreach文を使って実装してください。*/
+        static YearMonth GetFirst21(YearMonth[] vYearMonthArray) {
+            foreach (YearMonth wYearMonth in vYearMonthArray) {
+                return wYearMonth.Is21Century == true ? wYearMonth : null;
+            }
+            return null;
+        }
+    }
+}
---
+++ 章末問題4/章末問題4/章末問題4/YearMonth.cs
@@ -0,0 +1,51 @@
+﻿namespace 章末問題4 {
+    /*問題4.1
+    1.年(Year)と月(Month)の2つのプロパティを持つクラスYearMonthを定義してください。
+    このとき、2つのプロパティは読み取り専用にし、値はコンストラクタで指定できるようにしてください。
+    なお、引数で渡される月の値は1から12の範囲にあるものと仮定してかまいません。
+    */
+    class YearMonth {
+        /// <summary>
+        /// 年
+        /// </summary>
+        public int Year { get; private set; }
+        /// <summary>
+        /// 月
+        /// </summary>
+        public int Month { get; private set; }
+
+        /*2.YearMonthクラスに、Is21Centuryプロパティを追加してください。
+        2001年から2100年までが21世紀です。この処理では加減乗除は行わないでください。*/
+        /// <summary>
+        /// 21世紀
+        /// </summary>
+        public bool Is21Century {
+            get {
+                return Year >= 2001 && Year <= 2100;
+            }
+        }
+        //コンストラクタ
+        public YearMonth(int vYear, int vMonth) {
+            this.Year = vYear;
+            this.Month = vMonth;
+        }
+
+        /*3.YearMonthクラスに、1ヵ月後を求めるAddOneMonthメソッドを追加してください。
+        このとき、自分自身のプロパティは変更せずに、新たなYearMonthオブジェクトを生成しその値を返してください。
+        12月のときの処理に注意してください。*/
+        public YearMonth AddOneMonth(int vYearMonthArray) {
+            int wNewYear = Year;
+            int wNewMonth = Month + 1;
+            if (wNewMonth > 12) {
+                wNewMonth = 1;
+                wNewYear++;
+            }
+            return new YearMonth(wNewYear, wNewMonth);
+        }
+
+        //4.ToStringメソッドをオーバーライドしてください。結果は、"2017年8月"といった形式にしてください。
+        public override string ToString() {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
